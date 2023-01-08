c2be0add00e5c1a16b1a7f42f281df31210b4bb2
AlexJoki <stayinforest@gmail.com>
2023-01-08 21:41:10 +0200

    afreview

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function popUp() {
   let inputName = document.querySelector('.popup__input_type_name');
   let inputJob = document.querySelector('.popup__input_type_job');
-  document.querySelector('.popup').classList.toggle('popup__is-opened');
-  inputName.value = document.querySelector('.user-info__name').innerText;
-  inputJob.value = document.querySelector('.user-info__job').innerText;
+  document.querySelector('.popup').classList.toggle('popup_is-opened');
+  inputName.value = document.querySelector('.profile__name').innerText;
+  inputJob.value = document.querySelector('.profile__job').innerText;
 }
-document.querySelector('.user-info__edit-button').addEventListener('click', popUp);
+document.querySelector('.profile__edit-button').addEventListener('click', popUp);
 document.querySelector('.popup__close').addEventListener('click', popUp);
 
 
@@ -14,8 +14,8 @@ const editForm = document.forms.edit;
 
 function editProfile(event) {
   event.preventDefault();
-  let infoName = document.querySelector('.user-info__name');
-  let infoDescription = document.querySelector('.user-info__job');
+  let infoName = document.querySelector('.profile__name');
+  let infoDescription = document.querySelector('.profile__job');
   const nameForm = editForm.elements.name;
   const descriptionForm = editForm.elements.description;
   if (nameForm.value !== 0 && descriptionForm.value !== 0) {
